3b459839ba9bc6a50626863bc6d3c8d2bad91ffd
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-05-29 16:07:34 -0300

    Implementando método POST para criação de novos usuários.

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,11 +1,27 @@
 import { db } from "../db.js";
 
 export const getUsers = (_, res) => {
-    const query = "SELECT * FROM usuarios";
+    const q = "SELECT * FROM usuarios";
 
-    db.query(query, (err, data) => {
-        if(err) return res.json(err);
+    db.query(q, (err, data) => {
+        if (err) return res.json(err);
 
         return res.status(200).json(data);
+    }); 
+};
+
+export const addUser = (request, respose) => {
+    const query = "INSERT INTO usuarios(`nome`, `email`, `data_de_nascimento`)values(?)";
+
+    const values = [
+        request.body.nome,
+        request.body.email,
+        request.body.data_de_nascimento,
+    ];
+
+    db.query(query, [values], (err) => {
+        if (err) return respose.json(err);
+
+        return respose.status(200).json("Usuário criado com sucesso.");
     });
 };
