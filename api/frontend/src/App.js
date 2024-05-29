70f04d0b06dd05938737cc39930e13ca0b1c06b2
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-05-29 15:43:13 -0300

    Implementando tabela de usuários com método get

--- api/frontend/src/App.js
+++ api/frontend/src/App.js
@@ -2,8 +2,10 @@ import Global from "./style/global";
 import styled from "styled-components";
 import Form from "./components/Form.js";
 import Grid from "./components/Grid.js";
+import { useEffect, useState } from "react";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
 
 const Container = styled.div`
     width: 100%;
@@ -18,12 +20,28 @@ const Container = styled.div`
 const Title = styled.h2``;
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [onEdit, setOnEdit] = useState(null);
+
+  const getUsers = async () => {
+    try {
+      const res = await axios.get("http://localhost:8800");
+      setUsers(res.data.sort((a, b) => (a.nome > b.nome ? 1 : -1)));
+    } catch(error) {
+      toast.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, [setUsers]);
+
   return (
     <>
       <Container>
         <Title>USUÁRIOS</Title>
         <Form/>
-        <Grid/>
+        <Grid users={users}/>
       </Container>
       <ToastContainer autoClose={3000} position={toast.position}/>
       <Global/> 
---
+++ api/frontend/src/components/Grid.js
@@ -0,0 +1,76 @@
+import React from "react";
+import axios from "axios";
+import styled from "styled-components";
+import { FaTrash, FaEdit } from "react-icons/fa";
+import { toast } from "react-toastify";
+
+const Table = styled.table`
+    width: 100%;
+    background-color: #fff;
+    padding: 20px;
+    box-shadow: 0px 0px 5px #ccc;
+    box-radius: 5px;
+    margin: 20px auto;
+    word-brak: break-all;
+`;
+
+export const Thead = styled.thead``;
+
+export const Tbody = styled.tbody``;
+
+export const Tr = styled.tr``;
+
+export const Th = styled.th`
+    text-align: center;
+    border-bottom: inset;
+    padding-bottom: 5px;
+
+    @media (max-width: 500px) {
+        ${(props) => props.onlyWeb && "display:none"}
+    }
+`;
+
+export const Td = styled.td`
+    padding-top: 15px;
+    text-align: ${(props) => (props.alignCenter ? "center" : "start")};
+    width: ${(props) => (props.width ? props.width : "auto")};
+
+    @media (max-width: 500px) {
+        ${(props) => props.onlyWeb && "display :none"}
+    }
+`;
+
+const Grid = ({ users }) => {
+    return (
+        <Table>
+            <Thead>
+                <Tr>
+                    <Th>Nome</Th>
+                    <Th>Email</Th>
+                    <Th onlyWeb>Fone</Th>
+                    <Th></Th>
+                    <Th></Th>
+                </Tr>
+            </Thead>
+            <Tbody>
+                {users.map((item, i) => (
+                    <Tr key={i}>
+                        <Td width = "30%">{item.nome}</Td>
+                        <Td width = "30%">{item.email}</Td>
+                        <Td width = "20%" onlyWeb>
+                            {item.fone}
+                        </Td>
+                        <Td alignCenter width= "5%">
+                            <FaEdit />
+                        </Td>
+                        <Td alignCenter width="5%">
+                            <FaTrash />
+                        </Td>
+                    </Tr>
+                ))}
+            </Tbody>
+        </Table>
+    );
+};
+
+export default Grid;
