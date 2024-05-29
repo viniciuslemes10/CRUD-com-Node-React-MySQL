370c986eb9909275ea1d2b9f3f83ac4efcba32ce
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-05-29 15:57:43 -0300

    Implementando tabela de usuários com método get

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -9,3 +9,20 @@ export const getUsers = (_, res) => {
         return res.status(200).json(data);
     });
 };
+
+export const addUser = (req, res) => {
+    const query = "INSERT INTO usuarios(`nome`, `email`, `fone`, `data_nascimento`) VALUES(?)";
+
+    const values = [
+        req.body.nome,
+        req.body.email,
+        req.body.fone,
+        req.body.data_nascimento
+    ];
+
+    db.query(query, [values], (err) => {
+        if(err) return res.json(err);
+
+        return res.status(200).json("Usuário criado com sucesso.")
+    });
+} 
