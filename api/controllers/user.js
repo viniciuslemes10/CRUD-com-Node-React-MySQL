56eea65c4b1e873b3780bc47bc9dcf76a42ae099
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-05-29 16:10:45 -0300

    Arrumando implementação do método POST.

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -10,22 +10,6 @@ export const getUsers = (_, res) => {
     }); 
 };
 
-export const addUser = (request, respose) => {
-    const query = "INSERT INTO usuarios(`nome`, `email`, `data_de_nascimento`)values(?)";
-
-    const values = [
-        request.body.nome,
-        request.body.email,
-        request.body.data_de_nascimento,
-    ];
-
-    db.query(query, [values], (err) => {
-        if (err) return respose.json(err);
-
-        return respose.status(200).json("Usuário criado com sucesso.");
-    });
-};
-
 export const addUser = (req, res) => {
     const query = "INSERT INTO usuarios(`nome`, `email`, `fone`, `data_nascimento`) VALUES(?)";
 
